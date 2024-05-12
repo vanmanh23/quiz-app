6b680bcc13f9e7056c2a1b6817969c48e1ac6e4d
manhnguyen <manhcan03@gmail.com>
2024-05-12 22:32:56 +0700

    deploy to vercel

--- server/api/index.js
+++
@@ -1,7 +0,0 @@
-// import { VercelRequest, VercelResponse } from "@vercel/node";
-
-// export default async (req: VercelRequest, res: VercelResponse) => {
-//   return res.status(200).json({message: 'hello world'});
-// };
-
-export { default } from "../src/index.ts";
