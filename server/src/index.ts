72af3a82e0918791ec4c76e852e79c08df78337e
manhnguyen <manhcan03@gmail.com>
2024-05-20 10:40:21 +0700

    ..

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,34 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import { serve } from "@hono/node-server";
 import { router as categories } from './modules/categories/categories.controler';
 import { router as questions } from './modules/question/questions.controler';
-import { handle } from '@hono/node-server/vercel'
 
-const app = new Hono().basePath('/api');
+
+const app = new Hono().basePath("/api");
 
 app.use("*", logger());
 app.use(
-  "*",
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://quiz-app-api-one.vercel.app"
-    ],
-    credentials: true,
-  }),
+    "*",
+    cors({
+        origin: ["http://localhost:5173", "https://airbnbapp-web.vercel.app"],
+        credentials: true,
+    })
 );
 
 app.route("/categories", categories);
 app.route("/questions", questions);
 app.notFound((c) => {
-  return c.json({
-      message: "Not found",
-      statusCode: 404,
-  }, 404);
-});
+    return c.json(
+      {
+        message: "Not found",
+        statusCode: 404,
+      },
+      404,
+    );
+  });
 
 serve(app, () => {
-  console.log('Server is running on http://localhost:3000');
-});
-
-export default handle(app)
+    console.log("Server is running on http://localhost:3000");
+  });
