801d80cbec302ab18f83e09047ab6ec531900e3b
manhnguyen <manhcan03@gmail.com>
2024-05-24 23:45:50 +0700

    .

--- myserver/src/index.ts
+++ myserver/src/index.ts
@@ -11,7 +11,7 @@ app.use("*", logger());
 app.use(
     "*",
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173"],
         credentials: true,
     })
 );
