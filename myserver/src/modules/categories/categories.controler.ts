c9a4056182100086dff891f4416fc455f612f653
manhnguyen <manhcan03@gmail.com>
2024-05-25 16:57:39 +0700

    remove server folder

--- myserver/src/modules/categories/categories.controler.ts
+++ myserver/src/modules/categories/categories.controler.ts
@@ -9,29 +9,37 @@ router
     // return c.json({message: "Get all categories"});
     return c.json({message: "Get all categories", data: categories});
 })
-.get("/:id", async (c) => {
-    const id = c.req.param("id") ;
-    const category = await CategoriesService.getById(id);
-    return c.json({message: "Get category by id", data: category});
-})
 .post("/", async (c) => {
     const data = await c.req.json();
-    console.log(data);
-    await CategoriesService.create(data);
-    return c.json({message: "Create new category successfully"});
-})
-.post("/image", async (c) => {
-    const data = await c.req.json();
-    await CategoriesService.imagecreate(data);
-    return c.json({message: "Create new image successfully"});
-})
-.delete("/:id", async (c) => {
-    const id = c.req.param("id") ;
-    await CategoriesService.deleteById(id);
-    return c.json({message: "Delete category by id"});
-})
-.get("/getbytestname/:testName", async (c) => {
-    const testName = c.req.param("testName") ;
-    const categories = await CategoriesService.getByTestName(testName);
-    return c.json({message: "Get categories by test name", data: categories});
-})
+    const category = await CategoriesService.create(data);
+    return c.json({
+      data: category,
+      status: 200,
+    });
+  })
+// .get("/:id", async (c) => {
+//     const id = c.req.param("id") ;
+//     const category = await CategoriesService.getById(id);
+//     return c.json({message: "Get category by id", data: category});
+// })
+// .post("/", async (c) => {
+//     const data = await c.req.json();
+//     console.log(data);
+//     await CategoriesService.create(data);
+//     return c.json({message: "Create new category successfully"});
+// })
+// .post("/image", async (c) => {
+//     const data = await c.req.json();
+//     await CategoriesService.imagecreate(data);
+//     return c.json({message: "Create new image successfully"});
+// })
+// .delete("/:id", async (c) => {
+//     const id = c.req.param("id") ;
+//     await CategoriesService.deleteById(id);
+//     return c.json({message: "Delete category by id"});
+// })
+// .get("/getbytestname/:testName", async (c) => {
+//     const testName = c.req.param("testName") ;
+//     const categories = await CategoriesService.getByTestName(testName);
+//     return c.json({message: "Get categories by test name", data: categories});
+// })
--- myserver/src/modules/categories/categories.service.ts
+++ myserver/src/modules/categories/categories.service.ts
@@ -11,68 +11,73 @@ export class CategoriesService {
     }}});
     return data;
   }
-  static async getById(id: string) {
-    const data = await db.testName.findUnique({ where: { id }, include: {image: {
-      select: {
-        uri: true,
-        alt: true
-      }
-    }} });
-    return data;
-  }
-  static async create(data: TestName) {
-    try{
-    const result = await db.testName.create({ data });
+//   static async getById(id: string) {
+//     const data = await db.testName.findUnique({ where: { id }, include: {image: {
+//       select: {
+//         uri: true,
+//         alt: true
+//       }
+//     }} });
+//     return data;
+//   }
+//   static async create(data: TestName) {
+//     try{
+//     const result = await db.testName.create({ data });
 
-    return result;
-    }catch(err){
-      console.log(err);
-      throw err;
-    }
-  }
-  static async getByTestName(testName: string) {
-    const data = await db.testName.findFirst({where: {testName: testName}, include: {
-      image: {
-        select: {
-          uri: true,
-          alt: true
-        }
-      },
-      question: {
-        select: {
-          id: true,
-          question: true,
-          answerDescription: true,
-          hint: true,
-          options: true,
-          image: true
-        }
-      }
-    } })
-    return data;
-  }
-  static async deleteById(id: string) {
-    try{
-      const result = await db.testName.delete({ where: { id }, include: {image: {
-        select: {
-          uri: true,
-          alt: true
-        }
-      }}});
-    }
-     catch(err){
-      console.log(err);
-      throw err;
-    }
-}
-static async imagecreate(data: Image) {
-  try{
-  const result = await db.image.create({ data });
+//     return result;
+//     }catch(err){
+//       console.log(err);
+//       throw err;
+//     }
+//   }
+//   static async getByTestName(testName: string) {
+//     const data = await db.testName.findFirst({where: {testName: testName}, include: {
+//       image: {
+//         select: {
+//           uri: true,
+//           alt: true
+//         }
+//       },
+//       question: {
+//         select: {
+//           id: true,
+//           question: true,
+//           answerDescription: true,
+//           hint: true,
+//           options: true,
+//           image: true
+//         }
+//       }
+//     } })
+//     return data;
+//   }
+//   static async deleteById(id: string) {
+//     try{
+//       const result = await db.testName.delete({ where: { id }, include: {image: {
+//         select: {
+//           uri: true,
+//           alt: true
+//         }
+//       }}});
+//     }
+//      catch(err){
+//       console.log(err);
+//       throw err;
+//     }
+// }
+// static async imagecreate(data: Image) {
+//   try{
+//   const result = await db.image.create({ data });
 
-  return result;
-  }catch(err){
-    console.log(err);
-    throw err;
-  }
+//   return result;
+//   }catch(err){
+//     console.log(err);
+//     throw err;
+//   }
+// }
+
+static async create(data: TestName) {
+  const category = await db.testName.create({ data });
+  return category;
 }
 }
